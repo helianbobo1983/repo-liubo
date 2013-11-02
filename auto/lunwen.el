d8d6f601abb204e277d387882df07f00380c94c7
helianbobo1983 <105323647@qq.com>
2013-11-02 22:05:27 +0800

    添加了Latex文件

---
+++ auto/lunwen.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "lunwen"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "art12"
+     "article"
+     "a4paper"
+     "12pt"
+     "setup/package"
+     "format")))
+
